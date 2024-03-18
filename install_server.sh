fc1c28b5fe1d048154445ceed92689685d8d051f
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2024-03-17 22:42:39 -0400

    small change

--- install_server.sh
+++ install_server.sh
@@ -21,7 +21,7 @@ sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
 sudo chown -R csye6225 /etc/systemd/system/webapp.service
 sudo chgrp -R csye6225 /etc/systemd/system/webapp.service
 
-sudo chown -R csye6225:csye6225 /var/logs/webapplogs # logs folder for webapp
+sudo chown -R csye6225:csye6225 /var/log/webapplogs # logs folder for webapp
 
 sudo sed -i -e "s/enforcing/disabled/g" /etc/selinux/config # disabling SElinux
 sudo setenforce 0
