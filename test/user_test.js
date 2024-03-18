a5946349203288b64549ee0d8b0ad577b68f9546
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2024-03-17 21:12:35 -0400

    Added console transport to logger

--- test/user_test.js
+++ test/user_test.js
@@ -4,6 +4,10 @@ import { describe } from "mocha";
 import { initializeDB, sequelize } from "../models/index.js";
 import { expect } from "chai";
 import {faker} from "@faker-js/faker";
+import {logger, fileTransport, consoleTransport} from "../utils/logger.js";
+
+logger.remove(fileTransport);
+logger.add(consoleTransport);
 
 // Used faker package to generate mock data
 const mockUser = {
--- utils/logger.js
+++ utils/logger.js
@@ -4,9 +4,12 @@ const logger = winston.createLogger({
     level: "debug",
     format: winston.format.json(),
     defaultMeta: "wwebapp-service",
-    transports: [
-        new winston.transports.File({filename: "/var/log/webapplogs/webapp-logs", level: "debug"})
-    ]
+    transports: []
 });
 
-export default logger;
+const fileTransport = new winston.transports.File({filename: "/var/log/webapplogs/webapp-logs", level: "debug"});
+const consoleTransport = new winston.transports.Console();
+
+logger.add(fileTransport);
+
+export {logger, fileTransport, consoleTransport};
