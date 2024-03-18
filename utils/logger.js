9c64d60561b927c08ea49144d02ec7504f8ca495
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2024-03-17 22:29:02 -0400

    Added env check

--- utils/logger.js
+++ utils/logger.js
@@ -1,15 +1,38 @@
 import winston from "winston";
+// import dotenv from "dotenv";
+
+// dotenv.config();
 
 const logger = winston.createLogger({
     level: "debug",
-    format: winston.format.json(),
-    defaultMeta: "wwebapp-service",
+    format: winston.format.combine(
+        winston.format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+    
+        winston.format.json(),
+      ),
+    defaultMeta: "webapp-service",
     transports: []
 });
 
-const fileTransport = new winston.transports.File({filename: "/var/log/webapplogs/webapp-logs", level: "debug"});
-const consoleTransport = new winston.transports.Console();
+const consoleTransport = new winston.transports.Console({
+    name: "console",
+    colorize: true
+});
 
-logger.add(fileTransport);
+logger.add(consoleTransport);
+// console.log(process.env.NODE_ENV)
 
-export {logger, fileTransport, consoleTransport};
+if(process.env.NODE_ENV !== "test"){
+    logger.add(new winston.transports.File({
+        name: "file",
+        filename: "/var/log/webapplogs/webapp-logs",
+        colorize: true,
+        showLevel: true
+    }));
+}
+
+// logger.add(fileTransport);
+
+export default logger;
